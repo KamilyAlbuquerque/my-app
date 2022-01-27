c69891bd062577f1b88106d3732fbb72be4d9982
Kamily Albuquerque <kamily.albuquerque007@gmail.com>
2022-01-27 16:06:15 -0300

    MY-APP

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,8 @@
 import firebase from 'firebase/app';
 
+import 'firebase/auth';
+import 'firebase/database';
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -10,5 +13,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
-
+firebase.initializeApp(firebaseConfig)
 
