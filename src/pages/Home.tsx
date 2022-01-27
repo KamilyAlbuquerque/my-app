ab34455cdaa3b786a4ff30442f8069ec17106738
Kamily Albuquerque <kamily.albuquerque007@gmail.com>
2022-01-27 17:03:28 -0300

    MY-APP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,32 @@
-import illustrationImg from '../assets/images/'
+import illustration from '../assets/images/illustration.svg'
+import logo from '../assets/images/logo.svg';
+import googleIcon from '../assets/images/google-icon.svg';
 
 export function Home () {
     return (
         <div>
             <aside>
-               <img src="{illustrationImg}" alt="" />
+               <img src={illustration} alt="Ilustração simbolizando perguntas e respostas" />
+               <strong>Crie salas de Q&amp;A ao-vivo</strong>
+               <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
+            <main>
+                <div>
+                <img src={logo} alt="Letmeask" />
+                <button>
+                    <img src={googleIcon} alt="Logo do Google" />
+                    Crie sua sala com o Google 
+                </button>
+                <div>ou entre em uma sala</div>
+                <form>
+                    <input 
+                        type="text" 
+                        placeholder="Digite o código da sala"
+                    />
+                    <button type="submit"></button>
+                </form>
+                </div> 
+            </main>
         </div>
     )
 }
